43ade68d5e4a7d5a1a85352f616ba2f40fbbfd36
williamstyronejr <itenory@gmail.com>
2022-08-15 21:36:25 -0400

    Add create and join room

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,74 @@
-import React from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
+import Canvas from './Canvas';
 
 function App() {
-  return <section>Doodle App</section>;
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [input, setInput] = useState('');
+  const [doodleId, setDoodleId] = useState<string | null>(null);
+
+  const createRoom = useCallback(() => {
+    if (socket) socket.emit('createRoom');
+  }, [socket]);
+
+  const joinRoom = useCallback(() => {
+    if (socket) socket.emit('joinRoom');
+  }, [socket]);
+
+  useEffect(() => {
+    const newSocket = io();
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.close();
+    };
+  }, [setSocket]);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const roomCreate = (roomId: string) => {
+      setDoodleId(roomId);
+    };
+
+    const roomJoin = (success: boolean) => {
+      if (success) setDoodleId(input);
+    };
+
+    socket.on('roomCreated', roomCreate);
+    socket.on('roomJoin', roomJoin);
+
+    return () => {
+      socket.off('roomCreated', roomCreate);
+      socket.off('roomJoin', roomJoin);
+    };
+  }, [socket]);
+
+  return (
+    <section>
+      {doodleId ? (
+        <Canvas />
+      ) : (
+        <div>
+          <button onClick={() => createRoom()} type='button'>
+            Create Room
+          </button>
+
+          <div>
+            <input
+              type='text'
+              value={input}
+              onChange={(evt) => setInput(evt.target.value)}
+            />
+
+            <button onClick={() => joinRoom()} type='button'>
+              Join Room
+            </button>
+          </div>
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default App;
